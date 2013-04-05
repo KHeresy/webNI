b0605bf8296d3b606ece0a28cf77b447bf75a617
KHeresy <kheresy@gmail.com>
2013-04-05 16:45:24 +0800

    1. Add Boost Program Options. 2. Try to build a manager of connection for keeping state of each connection

---
+++ WebNI/ConnectionState.h
@@ -0,0 +1,6 @@
+#pragma once
+
+class ConnectionState
+{
+public:
+};
--- WebNI/main.cpp
+++ WebNI/main.cpp
@@ -17,9 +17,14 @@
 #include <OpenNI.h>
 #include <NiTE.h>
 
+// application Header
+#include "ConnectionState.h"
+
 #pragma endregion
 
-typedef websocketpp::server<websocketpp::config::asio> server;
+typedef websocketpp::config::asio														TConfig;
+typedef websocketpp::server<TConfig>													TServer;
+typedef websocketpp::endpoint<websocketpp::connection<TConfig>,TConfig>::connection_ptr	TConnection;
 
 #pragma region Global Objects
 // OpenNI and NiTE objects
@@ -27,9 +32,10 @@ openni::Device		g_Device;
 nite::UserTracker	g_UserTracker;
 
 // WebSocket++ Objects
-server*	g_pServer = NULL;
+TServer*	g_pServer = NULL;
 
 // Data
+std::map<TConnection,ConnectionState>	g_mapConnection;
 std::map<nite::UserId,nite::Skeleton>	g_UsersData;
 
 #pragma endregion
@@ -49,9 +55,30 @@ void ExitProgram()
 
 // Callback function of WebSocket++
 void onMessage(	websocketpp::connection_hdl hdl,
-				server::message_ptr msg )
+				TServer::message_ptr msg )
 {
+	TConnection mCon = g_pServer->get_con_from_hdl( hdl );
+	auto itCon = g_mapConnection.find( mCon );
+	if( itCon != g_mapConnection.end() )
+	{
+		
+	}
+	else
+	{
+		g_pServer->send( hdl, "Can't found mapping connection state.", websocketpp::frame::opcode::TEXT );
+	}
+	g_pServer->send( hdl, "Can't found mapping connection state.", websocketpp::frame::opcode::TEXT );
 	std::cout << msg->get_payload() << std::endl;
+}
+
+void onOpen( websocketpp::connection_hdl hdl )
+{
+	g_mapConnection.insert( std::make_pair( g_pServer->get_con_from_hdl( hdl ), ConnectionState() ) );
+}
+
+void onClose( websocketpp::connection_hdl hdl )
+{
+	g_mapConnection.erase( g_pServer->get_con_from_hdl( hdl ) );
 }
 
 void onTimer( const websocketpp::lib::error_code& e )
@@ -88,9 +115,40 @@ void onTimer( const websocketpp::lib::error_code& e )
 int main( int argc, char** argv )
 {
 	// Porgram Setting
-	int		iPort = 9002;
+	int			iPort;
+	std::string	sDevice	= "";//openni::ANY_DEVICE;
 
 	#pragma region Use boost::program_options to handle runtime setting
+	{
+		namespace BPO = boost::program_options;
+
+		// define program options
+		BPO::options_description bpoOptions( "Command Line Options" );
+		bpoOptions.add_options()
+			( "help,H",	BPO::bool_switch()->notifier( [&bpoOptions]( bool bH ){ if( bH ){ std::cout << bpoOptions << std::endl; exit(0); } } ),	"Help message" )
+			( "port,P", BPO::value(&iPort)->value_name("port_num")->default_value(9002),		"The port to listen" )
+			( "file,F",	BPO::value(&sDevice)->value_name("ONI_File"),							"Open an ONI file for test" );
+
+		// prase
+		try
+		{
+			BPO::variables_map mVMap;
+			BPO::store( BPO::parse_command_line( argc, argv, bpoOptions ), mVMap );
+			BPO::notify( mVMap );
+		}
+		catch( BPO::error_with_option_name e )
+		{
+			std::cerr << e.what() << std::endl;
+			std::cout << bpoOptions << std::endl;
+			return 1;
+		}
+		catch( BPO::error e )
+		{
+			std::cerr << e.what() << std::endl;
+			std::cout << bpoOptions << std::endl;
+			return 1;
+		}
+	}
 	// TODO: parse program options
 	#pragma endregion
 
@@ -105,7 +163,7 @@ int main( int argc, char** argv )
 
 	// Open OpenNI Device
 	std::cout << "Open OpenNI Device" << std::endl;
-	if( g_Device.open( openni::ANY_DEVICE ) != openni::STATUS_OK )
+	if( g_Device.open( sDevice.c_str() ) != openni::STATUS_OK )
 	{
 		std::cerr << " [ERROR] Can't open OpenNI Device: " << openni::OpenNI::getExtendedError() << std::endl;
 		return -1;
@@ -130,13 +188,15 @@ int main( int argc, char** argv )
 
 	#pragma region Initialize WebSocket++
 	// initialize
-	g_pServer = new server();
+	g_pServer = new TServer();
 	g_pServer->init_asio();
 	g_pServer->listen( iPort );
 	g_pServer->start_accept();
 
 	// set callback functions
 	g_pServer->set_message_handler( &onMessage );
+	g_pServer->set_open_handler( &onOpen );
+	g_pServer->set_close_handler( &onClose );
 	g_pServer->set_timer( 30, &onTimer );
 
 	// run
